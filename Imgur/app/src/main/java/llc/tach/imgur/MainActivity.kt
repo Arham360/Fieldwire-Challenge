4d84b1ad5d15f5752dedff2c61fe170933db9ac4
Arham Raza Akbari <arham360@gmail.com>
2020-06-17 00:30:05 -0500

    Hooking up to Imgur Api
    Was having some issues setting up retrofit so I decided to use the simplest possible way

--- Imgur/app/src/main/java/llc/tach/imgur/MainActivity.kt
+++ Imgur/app/src/main/java/llc/tach/imgur/MainActivity.kt
@@ -1,17 +1,25 @@
 package llc.tach.imgur
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.runBlocking
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: RecyclerAdapter
     private var recyclerView: RecyclerView? = null
+
+    private lateinit var httpClient: OkHttpClient
 
     var results = ArrayList<Imgur>()
 
@@ -29,27 +37,62 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onQueryTextSubmit(query: String): Boolean {
-                //getImagesBasedOnQuery(query)
+                runBlocking {
+                    getImagesBasedOnQuery(query)
+                }
+                adapter.notifyDataSetChanged()
                 return false
             }
         })
-
-        results.add(Imgur("dummy", "https://static01.nyt.com/images/2019/03/25/arts/25batman-DTC1000B/25batman-DTC1000B-videoSixteenByNineJumbo1600.jpg"))
 
         recyclerView = findViewById(R.id.recyclerView)
         linearLayoutManager = LinearLayoutManager(this)
         recyclerView?.layoutManager = linearLayoutManager
 
         adapter = RecyclerAdapter(results, this)
+        recyclerView?.adapter = adapter
         adapter.onItemClick = { image ->
             val intent = Intent(this, FullImageActivity::class.java).apply {
                 putExtra("Url", image.imageUrl)
             }
             startActivity(intent)
         }
+    }
 
-        recyclerView?.adapter = adapter
-        adapter.notifyDataSetChanged()
+    private fun getImagesBasedOnQuery(query: String) {
+        results.clear()
+
+        httpClient = OkHttpClient.Builder().build()
+
+        val request: Request = Request.Builder()
+            .url("https://api.imgur.com/3/gallery/search/{{sort}}/{{window}}/{{page}}?q=$query")
+            .header("Authorization", "Client-ID eb6206e6b72a898")
+            .header("User-Agent", "Imgur")
+            .build()
+
+        httpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException) {
+                Log.e("Error", "An error has occurred $e")
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call?, response: Response?) {
+                val data = JSONObject(response!!.body()!!.string())
+                val items = data.getJSONArray("data")
+
+                for (i in 0 until items.length()) {
+                    val item = items.getJSONObject(i)
+
+                    val title = item.getString("title")
+                    val url = item.getString("link")
+                    val imgur = Imgur(title, url)
+
+                    Log.v(imgur.title, imgur.imageUrl)
+
+                    results.add(imgur)
+                }
+            }
+        })
 
     }
 }
